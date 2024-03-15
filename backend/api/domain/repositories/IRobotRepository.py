9a3a02447578b0edee9a83d65ed1d45cf9cd5fdf
Jonathan Aires <airesjonathan123@gmail.com>
2024-03-15 14:27:22 -0300

    feat: create repositories (using abstract methods)

--- backend/api/domain/repositories/IRobotRepository.py
+++ backend/api/domain/repositories/IRobotRepository.py
@@ -1,18 +1,25 @@
 from api.domain.entities.Robot import Robot
+from abc import ABC, abstractmethod
 
 # Interface for Robot repository
-class IRobotRepository:
+class IRobotRepository(ABC):
+    
+    @abstractmethod
     def create(self, robot: Robot) -> Robot:
         pass
     
+    @abstractmethod
     def update(self, id: str, newRobot: Robot) -> bool:
         pass
     
+    @abstractmethod
     def delete(self, id: str) -> bool:
         pass
     
+    @abstractmethod
     def findById(self, id: str) -> Robot:
         pass
     
+    @abstractmethod
     def findAll() -> list[Robot]:
         pass
--- backend/api/domain/repositories/IUserRepository.py
+++ backend/api/domain/repositories/IUserRepository.py
@@ -1,18 +1,25 @@
 from api.domain.entities.User import User
+from abc import ABC, abstractmethod
 
 # Interface for User repository
-class IUserRepository:
+class IUserRepository(ABC):
+    
+    @abstractmethod
     def create(self, user: User) -> User:
         pass
     
+    @abstractmethod
     def update(self, id: str, newUser: User) -> bool:
         pass
     
+    @abstractmethod
     def delete(self, id: str) -> bool:
         pass
     
+    @abstractmethod
     def findById(self, id: str) -> User:
         pass
     
+    @abstractmethod
     def findAll() -> list[User]:
         pass
