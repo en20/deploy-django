1c21b022b055d1fabadcfd8215daf04cdebf714a
Jonathan Aires <airesjonathan123@gmail.com>
2024-03-14 21:49:18 -0300

    feat: create repositories
    feat: create repositories
    
    feat: create repositories (using abstract methods)

---
+++ backend/api/domain/repositories/ILogRepository.py
@@ -0,0 +1,25 @@
+from api.domain.entities.log import Log
+from abc import ABC, abstractmethod
+
+# Interface for Log repository
+class ILogRepository(ABC):
+    
+    @abstractmethod
+    def create(self, log: Log) -> Log:
+        pass
+    
+    @abstractmethod
+    def update(self, id: str, newLog: Log) -> bool:
+        pass
+    
+    @abstractmethod
+    def delete(self, id: str) -> bool:
+        pass
+
+    @abstractmethod
+    def findById(self, id: str) -> Log:
+        pass
+    
+    @abstractmethod
+    def findAll() -> list[Log]:
+        pass
