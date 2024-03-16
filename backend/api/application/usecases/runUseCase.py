7b163d20c77e2588e48fe7090e18067ec7507a16
Lu1z-Gust4v0 <gustavo1204.gc@gmail.com>
2024-03-16 19:31:54 -0300

    refact: add 'count' function instead of using a for loop

--- backend/api/application/usecases/runUseCase.py
+++ backend/api/application/usecases/runUseCase.py
@@ -2,20 +2,15 @@ from api.application.ports.runPort import RunPort
 from api.domain.entities.run import Run
 from api.adapters.outbound.database.repositories.RunRepository import RunRepository
 
+
 class RunUseCase(RunPort):
     runRepository: RunRepository
-    
+
     def __init__(self, runRepository: RunRepository) -> None:
         self.runRepository = runRepository
-    
+
     def getRobotRun(self, robot, skip, limit) -> list[Run]:
-       return self.runRepository.getRobotRuns(robot=robot)[skip:limit]
-        
+        return self.runRepository.getRobotRuns(robot=robot)[skip:limit]
+
     def countRobotRuns(self, robot) -> int:
-        count = 0
-        runs = self.runRepository.getRobotRuns(robot=robot)
-        
-        for run in runs:
-            count += 1
-        
-        return count
+        return self.runRepository.countRobotRuns(robot)
