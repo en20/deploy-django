9b1499c2fbca52618644c39bdfc0c589ab42b31d
Lu1z-Gust4v0 <gustavo1204.gc@gmail.com>
2024-03-31 15:10:22 -0300

    feat: find all by groups function

--- backend/api/adapters/outbound/database/repositories/RobotRepository.py
+++ backend/api/adapters/outbound/database/repositories/RobotRepository.py
@@ -1,19 +1,21 @@
-from api.adapters.outbound.database.models.robot import Robot
+from api.adapters.outbound.database.models.robot import Robot as RobotSchema
 from api.domain.repositories.IRobotRepository import IRobotRepository
 from django.core.exceptions import ObjectDoesNotExist
+from api.domain.entities.robot import Robot
+
 
 # Concrete Implementation for Robot Repository
 class RobotRepository(IRobotRepository):
     def create(self, robot: Robot) -> Robot:
         return Robot.objects.create(
-            id=robot.id, 
-            name=robot.name, 
-            description=robot.description, 
+            id=robot.id,
+            name=robot.name,
+            description=robot.description,
             section_name=robot.section_name,
             group=robot.group,
             created_at=robot.created_at
         )
-    
+
     def update(self, id, newRobot: Robot) -> bool:
         try:
             Robot.objects.filter(id=id).update(
@@ -27,16 +29,32 @@ class RobotRepository(IRobotRepository):
             return True
         except ObjectDoesNotExist:
             return False
-    
+
     def delete(self, id) -> bool:
         try:
             Robot.objects.filter(id=id).delete()
             return True
         except ObjectDoesNotExist:
             return False
-    
+
     def findById(self, id) -> Robot:
-        return Robot.objects.get(id=id)
-    
-    def findAll() -> list[Robot]:
-        return Robot.objects.all()
+        return self.schemaToRobot(Robot.objects.get(id=id))
+
+    def findAll(self, skip, limit) -> list[Robot]:
+        return map(self.schemaToRobot, Robot.objects.all()[skip:limit])
+
+    def findAllByGroups(self, groups: list[str]) -> list[Robot]:
+        return map(
+            self.schemaToRobot,
+            filter(lambda robot: robot.group.id in groups, Robot.objects.all()),
+        )
+
+    def schemaToRobot(schema: RobotSchema) -> Robot:
+        return Robot(
+            schema.id,
+            schema.name,
+            schema.description,
+            schema.section_name,
+            schema.group,
+            schema.created_at,
+        )
