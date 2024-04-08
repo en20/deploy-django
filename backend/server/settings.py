a67226596c7883e9b9d7e46d755790e1b12f7521
Lu1z-Gust4v0 <gustavo1204.gc@gmail.com>
2024-04-07 23:27:24 -0300

    chore: remove duplicated fields in settings

--- backend/server/settings.py
+++ backend/server/settings.py
@@ -58,7 +58,6 @@ MIDDLEWARE = [
     "django.middleware.common.CommonMiddleware",
 ]
 
-
 ROOT_URLCONF = "server.urls"
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
@@ -67,17 +66,6 @@ CSRF_TRUSTED_ORIGINS = [
     "http://127.0.0.1:3000",
     "http://localhost:3000",
 ]
-
-ROOT_URLCONF = "server.urls"
-CORS_ORIGIN_ALLOW_ALL = True
-CORS_ALLOW_CREDENTIALS = True
-CSRF_TRUSTED_ORIGINS = [
-    "http://0.0.0.0:3000",
-    "http://127.0.0.1:3000",
-    "http://localhost:3000",
-]
-
-ROOT_URLCONF = "server.urls"
 
 TEMPLATES = [
     {
