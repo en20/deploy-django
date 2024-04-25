b4fac591b1ef4529143aaa880f25e1928b3ce8a7
seth <alanzcc1@gmail.com>
2024-04-25 14:57:40 -0300

    fix: fixed imports

--- backend/api/tests/unit/test_group_repository.py
+++ backend/api/tests/unit/test_group_repository.py
@@ -1,18 +1,20 @@
-from django.test import SimpleTestCase
+from django.test import TestCase
 
-from backend.api.adapters.outbound.database.models.group import Group
-from backend.api.adapters.outbound.database.repositories.GroupRepository import GroupRepository
+from api.adapters.outbound.database.models.group import Group
+from api.adapters.outbound.database.repositories.GroupRepository import (
+    GroupRepository,
+)
 
 
-class TestGroupRepository(SimpleTestCase):
+group_repo = GroupRepository()
 
-    def setUp(self):
-        self.group_repo = GroupRepository()
 
-    def test_group_creation(self):
+class TestGroupRepository(TestCase):
+
+    def test_group_create(self):
 
         # Arrange
-        test_group = self.group_repo.create(name="autobots", description="this is a test")
+        test_group = group_repo.create(name="autobots", description="this is a test")
 
         # Act and Assert
         self.assertIsNotNone(test_group)
@@ -22,55 +24,51 @@ class TestGroupRepository(SimpleTestCase):
     def test_group_update(self):
 
         # Arrange
-        test_group = self.group_repo.create(name="autobots", description="this is a test")
+        test_group = group_repo.create(name="autobots", description="this is a test")
 
         # Act
-        self.group_repo.update(test_group, "the_jons", "not being baianos")
+        u_result = group_repo.update(test_group.id, "the_jons", "not being baianos")
+        id_result = group_repo.findById(test_group.id)
 
         # Assert
-        self.assertEqual(test_group.name, "the_jons")
-        self.assertEqual(test_group.description, "not being baianos")
+        self.assertEqual(True, u_result)
+        self.assertEqual(id_result.name, "the_jons")
+        self.assertEqual(id_result.description, "not being baianos")
 
     def test_group_delete(self):
         # Arrange
-        test_group = self.group_repo.create(name="autobots", description="this is a test")
+        test_group = group_repo.create(name="autobots", description="this is a test")
 
         # Act
-        self.group_repo.delete(test_group)
+        result = group_repo.delete(test_group.id)
 
         # Assert
-        self.assertIsNone(test_group)
-
-    # Do I need to make test_group an entity_group?
-    def test_group_schema_to_group(self):
-        # Arrange
-        test_group = self.group_repo.create(name="autobots", description="this is a test")
-
-        # Act
-        entity_group = self.group_repo.schemaToGroup(test_group)
-
-        # Assert
-        self.assertIsNotNone(entity_group)
+        self.assertEqual(True, result)
 
     def test_group_find_by_id(self):
 
         # Arrange
-        test_group = self.group_repo.create(name="autobots", description="this is a test")
+        test_group = group_repo.create(name="autobots", description="this is a test")
 
-        # Act and Assert
-        self.assertEqual(self.group_repo.findById(test_group), test_group)
+        # Act
+        result = group_repo.findById(test_group.id)
+
+        # Assert
+        self.assertEqual(test_group, result)
 
     def test_group_find_all(self):
 
         # Arrange
-        test_group_one = self.group_repo.create(name="autobots", description="this is a test")
-        test_group_two = self.group_repo.create(name="autobots", description="this is a test")
+        test_group_one = group_repo.create(
+            name="autobots", description="this is a test"
+        )
+        test_group_two = group_repo.create(
+            name="autobots", description="this is a test"
+        )
 
         # Act
-        check_list = self.group_repo.findAll(Group, 2)
+        check_list = group_repo.findAll(0, 2)
 
         # Assert
         self.assertEquals(check_list[0], test_group_one)
         self.assertEquals(check_list[1], test_group_two)
-
-
